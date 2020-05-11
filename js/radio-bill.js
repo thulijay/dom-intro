85a3374f88e05f5c66a14a85413c33115e3bd7fc
thulijay <thulijay79@gmail.com>
2020-05-11 08:20:17 +0200

    checking

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,17 +1,16 @@
 // get a reference to the sms or call radio buttons
-var radioType = document.querySelector('.billItemTypeRadio')
 //get a reference to the add button
 var radioButton = document.querySelector('.radioBillAddBtn')
 //create a variable that will keep track of the total bill
 var radioCalls = document.querySelector('.callTotalTwo')
 var radioSms = document.querySelector('.smsTotalTwo')
-var radioTotal = document.querySelector('totalTwo')
+var radioTotal = document.querySelector('.totalTwo')
 
 var smsTotal = 0;
 var callsTotal = 0;
 
 //add an event listener for when the add button is pressed
-radioButton.addEventListener('click', calculateBtnClicked)
+radioButton.addEventListener('click', radioBill)
 //in the event listener get the value from the billItemTypeRadio radio buttons
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
@@ -31,8 +30,9 @@ var billItemType = checkedRadioBtn.value;
     radioCalls.innerHTML = (callsTotal).toFixed(2);
     radioSms.innerHTML = (smsTotal).toFixed(2);
     var currentTotal = callsTotal + smsTotal;
-    radioTotal.innerHTML = (currentTotal).toFixed(2);
+  }
 
+    function styleTotalColor(){
   radioTotal.classList.remove('danger')
   radioTotal.classList.remove('warning')
 
