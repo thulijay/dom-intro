3e3a7f5364e46e3a60d6d2e891507982da206347
thulijay <thulijay79@gmail.com>
2020-05-13 08:47:07 +0200

    fixed

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -8,9 +8,8 @@ var radioTotal = document.querySelector('.totalTwo')
 
 var smsTotal2 = 0;
 var callsTotal2 = 0;
-
+var billTotal2 = 0;
 //add an event listener for when the add button is pressed
-radioButton.addEventListener('click', radioBill)
 //in the event listener get the value from the billItemTypeRadio radio buttons
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
@@ -20,27 +19,30 @@ var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked
 var billItemType = checkedRadioBtn.value;
 
     if (billItemType === "call"){
+      billTotal2 += 2.75;
       callsTotal2 += 2.75;
     }
 
     else if (billItemType === "sms"){
+      billTotal2 += 0.75;
       smsTotal2 += 0.75;
     }
 
     radioCalls.innerHTML = (callsTotal2).toFixed(2);
     radioSms.innerHTML = (smsTotal2).toFixed(2);
-    var radioT = callsTotal2 + smsTotal2;
-    radioTotal.innerHTML = radioT.toFixed(2);
+    radioTotal.innerHTML = billTotal2.toFixed(2);
+    styleColors();
+  }
 
     function styleColors(){
-  radioTotal.classList.remove('danger')
-  radioTotal.classList.remove('warning')
+  //radioTotal.classList.remove('danger')
+  //radioTotal.classList.remove('warning')
 
-  if (radioT >= 50){
+  if (billTotal2 >= 50){
     radioTotal.classList.add('danger')
   }
-  else if (radioT >= 30 && radioT < 50){
+  else if (billTotal2 >= 30 && billTotal2 < 50){
     radioTotal.classList.add('warning')
   }
 }
-}
+radioButton.addEventListener('click', radioBill)
